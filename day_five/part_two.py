8b67212e0d6e52defcb5d41813f15ea548abff85
JayOneTheSk8 <misterjustincox@gmail.com>
2021-12-12 13:14:27 -0500

    * complete day two * add option to count diagonals in vent_grid

---
+++ day_five/part_two.py
@@ -0,0 +1,24 @@
+from vent_manager.vent_grid import VentGrid
+
+def get_overlapping_lines():
+    line_segments = []
+
+    with open('day_five/input.txt') as f:
+        lines = f.read().splitlines()
+        for s in lines:
+            line_segment = []
+
+            line_points = s.split(' -> ')
+            line_segment.append([int(coord) for coord in line_points[0].split(',')])
+            line_segment.append([int(coord) for coord in line_points[1].split(',')])
+
+            line_segments.append(line_segment)
+
+    vent_grid = VentGrid(count_diagonals=True)
+
+    for line in line_segments:
+        vent_grid.mark_vent(line)
+
+    return vent_grid.get_points_where_lines_overlap()
+
+print(get_overlapping_lines())
--- day_five/vent_manager/vent_grid.py
+++ day_five/vent_manager/vent_grid.py
@@ -2,8 +2,9 @@ COUNT = 'count'
 
 class VentGrid():
 
-    def __init__(self, row_count=1000):
+    def __init__(self, row_count=1000, count_diagonals=False):
         self.__create_grid(int(row_count))
+        self.__count_diagonals = count_diagonals
 
     def mark_vent(self, line):
         if not self.__is_list(line) or not self.__is_list(line[0]) or not self.__is_list(line[1]):
@@ -12,8 +13,20 @@ class VentGrid():
         if len(line) != 2 or len(line[0]) != 2 or len(line[1]) != 2:
             raise Exception('Lines must be two points')
 
+        # [ [1, 1], [3, 3] ]
+        if line[0][0] == line[0][1] and line[1][0] == line[1][1]:
+            if self.__count_diagonals:
+                self.__handle_same_number_diagonal(line[0][0], line[1][0])
+        
+        # [ [9, 7], [7, 9] ]
+        elif line[0][0] == line[1][1] and line[0][1] == line[1][0]:
+            if self.__count_diagonals:
+                start_point = line[0][0]
+                end_point = line[0][1]
+                self.__handle_swap_num_diagonal(min(start_point, end_point), max(start_point, end_point))
+
         # [ [2, 1], [2, 7] ]
-        if line[0][0] == line[1][0]:
+        elif line[0][0] == line[1][0]:
             x = line[0][0]
             y1 = line[0][1]
             y2 = line[1][1]
@@ -26,9 +39,33 @@ class VentGrid():
             x2 = line[1][0]
             self.__handle_horizontal_line(y, min(x1, x2), max(x1, x2))
 
+        # [ [2, 0], [6, 4]]
+        elif line[0][0] - line[1][0] == line[0][1] - line[1][1]:
+            if self.__count_diagonals:
+                larger_nums = line[1]
+                smaller_nums = line[0]
+
+                if max(line[0][0], line[1][0]) == line[0][0]:
+                    larger_nums = line[0]
+                    smaller_nums = line[1]
+
+                self.__handle_growing_diagonal(smaller_nums[0], smaller_nums[1], larger_nums[0], larger_nums[1])
+
+        # [ [8, 2], [5, 5] ]
+        elif abs(line[0][0] - line[1][0]) == abs(line[0][1] - line[1][1]):
+            if self.__count_diagonals:
+                larger_nums = line[1]
+                smaller_nums = line[0]
+
+                if max(line[0][0], line[1][0]) == line[0][0]:
+                    larger_nums = line[0]
+                    smaller_nums = line[1]
+
+                self.__handle_shrinking_diagonal(larger_nums[0], larger_nums[1], smaller_nums[0], smaller_nums[1])
+
         else:
             pass
-    
+
     def print_rows(self):
         for row in self.__rows:
             print(row)
@@ -43,7 +80,7 @@ class VentGrid():
                 if vent_marker[COUNT] > 1:
                     count += 1
 
-        return count 
+        return count
 
     def __handle_vertical_line(self, x, start_y, end_y):
         for y in range(start_y, end_y + 1):
@@ -52,6 +89,33 @@ class VentGrid():
     def __handle_horizontal_line(self, y, start_x, end_x):
         for x in range(start_x, end_x + 1):
             self.__rows[y][x][COUNT] += 1
+
+    def __handle_growing_diagonal(self, x, y, limit_x, limit_y):
+        iter_x = x
+        iter_y = y
+
+        while iter_x <= limit_x and iter_y <= limit_y:
+            self.__rows[iter_y][iter_x][COUNT] += 1
+
+            iter_x += 1
+            iter_y += 1
+
+    def __handle_shrinking_diagonal(self, x, y, lower_limit_x, lower_limit_y):
+        iter_x = x
+        iter_y = y
+
+        while iter_y <= lower_limit_y and iter_x >= lower_limit_x:
+            self.__rows[iter_y][iter_x][COUNT] += 1
+
+            iter_x -= 1
+            iter_y += 1
+
+    def __handle_swap_num_diagonal(self, start_point, end_point):
+        offset = 0
+
+        for row_idx in range(start_point, end_point + 1):
+            self.__rows[row_idx][end_point - offset][COUNT] += 1
+            offset += 1
 
     def __handle_same_number_diagonal(self, start_num, end_num):
         for point in range(start_num, end_num + 1):
